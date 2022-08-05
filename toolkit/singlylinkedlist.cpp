75069aeff496591f794011b38a39fd9b5d71aa8c
ecomitz <ericcomitz@gmail.com>
2022-08-05 18:37:36 -0400

    More implementation to add for singly linked list. Also a test to see what user account gets commit credit

--- toolkit/singlylinkedlist.cpp
+++ toolkit/singlylinkedlist.cpp
@@ -1,9 +1,10 @@
 #include "singlylinkedlist.h"
 
-
+// All scenarios need test cases
 
 	void singlylinkedlist::add(int data)
 	{
+		// Scenario: No nodes exist yet
 		if (head == 0)
 		{
 			head = new node;
@@ -11,6 +12,7 @@
 			tail = head;
 			return;
 		}
+		// Scenario: Only 1 node exists
 		else if (tail == head)
 		{
 			tail = new node;
@@ -20,11 +22,17 @@
 			return;
 		}
 		
-		
-			node *traverser;
+		 // Scenario: There is already 2 nodes present. 
+			node *traverser = head;
 			node* trailer;
-
-
-		
+			while (traverser != 0)
+			{
+				trailer = traverser;
+				traverser = traverser->next;			
+			}
+			traverser = new node;
+			traverser->data = data;
+			traverser->next = 0;
+			trailer->next = traverser;
 	
 	}
